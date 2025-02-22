195866a108ad5c5a6d8471ac9a8dfbca9a2df8a6
Kevin Pintar <kevinpintar@hotmail.fr>
2025-02-22 23:08:37 +0100

    Added paragraph to Lost component

--- my-app/app/lost/page.tsx
+++ my-app/app/lost/page.tsx
@@ -8,13 +8,15 @@ export default function LostPage() {
     const router = useRouter();
 
     useEffect(() => {
-        setTimeout(() => {
+        const timer = setTimeout(() => {
         router.push("/");
         }, 5000);
-    }, []);
+        return () => clearTimeout(timer);
+    }, [router]);
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-900">
+        <div className="flex justify-center flex-col items-center min-h-screen">
+            <p className="text-4xl font-bold text-purple-200">I'm lost</p>
             <img src="/lost.gif" alt="GIF animé" className="w-64 h-64" />
         </div>
     ) 
