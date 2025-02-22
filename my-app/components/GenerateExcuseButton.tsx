0f9fed2706a47ae5cb6e9e00aaf442b6db835bf8
Kevin Pintar <kevin.pintar@epitech.eu>
2025-02-23 00:59:20 +0100

    Created ButtonLoader component and implemented function to generate and display an excuse

--- my-app/components/GenerateExcuseButton.tsx
+++ my-app/components/GenerateExcuseButton.tsx
@@ -1,9 +1,15 @@
 'use client';
+import { Excuse } from "@/types/excuse";
 import { Button } from "@heroui/react";
 import { useEffect, useState } from "react";
-
-export default function GenerateExcuseButton() {
-    const [excuses, setExcuses] = useState([]);
+import ButtonLoader from "./ButtonLoader";
+interface GenerateExcuseButtonProps {
+  onExcuseGenerated: (excuse: Excuse) => void; // Callback pour envoyer l'excuse au parent
+}
+export default function GenerateExcuseButton({ onExcuseGenerated }: GenerateExcuseButtonProps) {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [excuses, setExcuses] = useState<Excuse[]>([]);
+    const [usedExcuses, setUsedExcuses] = useState<Excuse[]>([]);
 
     useEffect(() => {
         fetch('http://localhost:3000/api/excuses?all=true')
@@ -14,9 +20,42 @@ export default function GenerateExcuseButton() {
           })
           .catch(error => console.error('Error fetching excuses:', error));
     }, []);
-    console.log('ok');
     
+    const handleClick = () => {
+        setIsLoading(true);
+        if (excuses.length === 0) return; 
+
+        // Filtre des excuses non utilisées
+        const unusedExcuses = excuses.filter(excuse => !usedExcuses.includes(excuse));
+
+        // Si toutes les excuses ont été utilisées, réinitialiser l'état
+        if (unusedExcuses.length === 0) {
+            setUsedExcuses([]);
+            alert('Toutes les excuses ont été utilisées. Réinitialisation...');
+            return;
+        }
+
+        // Choisir une excuse aléatoire parmi celles qui n'ont pas encore été utilisées
+        const randomIndex = Math.floor(Math.random() * unusedExcuses.length);
+        const randomExcuse = unusedExcuses[randomIndex];
+
+        // Mettre à jour l'état pour l'excuse actuelle et marquer cette excuse comme utilisée
+        const timer = setTimeout(() => {
+          setIsLoading(false);
+          onExcuseGenerated(randomExcuse);
+          setUsedExcuses(prevUsedExcuses => [...prevUsedExcuses, randomExcuse]);
+          return () => clearTimeout(timer);
+        }, 1000);
+    };
+
     return (
-        <Button color="secondary" size="lg">Trouver une excuse</Button>
-    );
+        isLoading ? 
+        (
+            <ButtonLoader />
+        ) : (
+              <Button color="secondary" size="lg" onPress={handleClick}>
+                  Générer une excuse
+              </Button>
+            )
+    )
 }
